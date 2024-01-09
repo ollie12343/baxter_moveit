7a8c353cc1f84d3a327eeab53668af4160e908f1
ollie12343 <87453519+ollie12343@users.noreply.github.com>
2024-01-09 11:12:16 +1100

    Update baxter.sh

--- baxter.sh
+++ baxter.sh
@@ -24,7 +24,7 @@ baxter_hostname="011312P0012.local"
 # Set *Either* your computers ip address or hostname. Please note if using
 # your_hostname that this must be resolvable to Baxter.
 #your_ip="192.168.XXX.XXX"
-your_hostname="my_computer.local"
+your_hostname="myhostname.local"
 
 # Specify ROS distribution (e.g. indigo, hydro, etc.)
 ros_version="kinetic"
